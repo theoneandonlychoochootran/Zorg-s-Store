c1420a8df765268671e8772e343308e3475f4a39
jonbrashears <36751060+jonbrashears@users.noreply.github.com>
2018-03-19 04:27:15 -0500

    Add files via upload
    Updated scaling, added font style, messed around with random things

--- ZorgStore/navigation.php
+++ ZorgStore/navigation.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Zorg Books</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="bootstrap-4.0.0-dist/css/bootstrap.min.css"/>
+  <script src="bootstrap-4.0.0-dist/js/bootstrap.min.js"></script>
+   <link rel="stylesheet" type="text/css" href="mystyle.css">
+</head>
 <div class="jumbotron">
   <div class="container text-center">
 	<h1>Zorg Books!</h1>      
@@ -8,12 +18,12 @@
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
       <ul class="nav navbar-nav navbar-left">
-        <li><a href="#"> Shopping Cart:</a></li>
+		<button type="button" class="btn btn-link">Shopping Cart</button>
       </ul>
 	  <ul class="nav navbar-nav navbar-right">
 		<form class="form-inline">
 		<input type="email" class="form-control" size="30" placeholder="Search">
-		<button type="button" class="btn btn-primary"> Search </button>
+		<button type="button" class="btn btn-basic"> Search </button>
 	  </ul>
   </form>
     </div>
--- ZorgStore/zorghome.php
+++ ZorgStore/zorghome.php
@@ -1,46 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Zorg Books</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="bootstrap-4.0.0-dist/css/bootstrap.min.css"/>
-  <script src="bootstrap-4.0.0-dist/js/bootstrap.min.js"></script>
-  <style>
-    
-     .jumbotron {
-	  background-color: #3F47CC;
-	  padding: 100px;
-      margin-bottom: 0;
-	  border-radius: 0;
-    }
-	
-	.jumbotron h1{
-	 color: pink;
-	}
-	
-   .navbar {
-	  background-color: #3F47CC;
-	  margin-bottom: 8%;
-	  padding-top: 10px;
-	  padding-bottom: 10px;
-	  padding-right: 20px;
-      border-radius: 0;
-    }
-	
-    footer {
-      background-color: #fEF200;
-	  position: fixed;
-	  bottom: 0;
-	  width: 100%;
-	  padding: 5px;
-    }
-	
-  </style>
-</head>
 
-<body>
 <?php include 'navigation.php';?>
+<body>
+
+<div class="container">
+	<div class = "panel panel-primary">
+		<h1>Featured Books</h1>
+		</div>
+	</div>
+
 <div class="container">    
   <div class="row">
     <div class="col-sm-4">
