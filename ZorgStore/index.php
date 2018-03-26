7aa8723c87f1858af5dcfdb8e3069a278904297b
jonbrashears <36751060+jonbrashears@users.noreply.github.com>
2018-03-26 14:14:10 -0500

    Featured books now display correctly

--- ZorgStore/index.php
+++ ZorgStore/index.php
@@ -10,13 +10,28 @@
 <div class="container">    
   <div class="row">
     <div class="col-sm-4">
-      <a href="product.php?id=1" title =""><img src="productsample.jpg" class="img-responsive" style="width:50%" alt="Image"></a>
+	<?php $id = 1;
+	  $sql = "SELECT book_pic FROM books WHERE book_id='1'";
+	  $result = $conn->query($sql);
+	  $row = $result->fetch_assoc();
+	?>
+      <a href="product.php?id=<?php echo $id ?>"> <?php echo '<img src="data:image/jpeg;base64,'.base64_encode( $row["book_pic"]).'" width="160" height="240"/>';?> </a>
     </div>
     <div class="col-sm-4"> 
-      <a href="product.php?id=2" title =""><img src="productsample.jpg" class="img-responsive" style="width:50%" alt="Image"></a>
+      <?php $id = 2;
+	  $sql = "SELECT book_pic FROM books WHERE book_id='$id'";
+	  $result = $conn->query($sql);
+	  $row = $result->fetch_assoc();
+	?>
+      <a href="product.php?id=<?php echo $id ?>"> <?php echo '<img src="data:image/jpeg;base64,'.base64_encode( $row["book_pic"]).'" width="160" height="240"/>';?> </a>
     </div>
     <div class="col-sm-4"> 
-      <a href="product.php?id=3" title =""><img src="productsample.jpg" class="img-responsive" style="width:50%" alt="Image"></a>
+      <?php $id = 3;
+	  $sql = "SELECT book_pic FROM books WHERE book_id='$id'";
+	  $result = $conn->query($sql);
+	  $row = $result->fetch_assoc();
+	?>
+      <a href="product.php?id=<?php echo $id ?>"> <?php echo '<img src="data:image/jpeg;base64,'.base64_encode( $row["book_pic"]).'" width="160" height="240"/>';?> </a>
     </div>
   </div>
 
