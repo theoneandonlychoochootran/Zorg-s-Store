b9f722d90357352a1e10c7a9e38ead822e91affb
jonbrashears <36751060+jonbrashears@users.noreply.github.com>
2018-04-09 17:30:14 -0500

    Author index page

---
+++ ZorgStore/authorindex.php
@@ -0,0 +1,42 @@
+<?php include 'navigation.php';?>
+
+<body>
+<?php
+
+$sql = "SELECT * FROM authors" ;
+
+$result = $conn->query($sql); ?>
+
+<div class="container fb-jb">
+	<div class = "panel panel-primary">
+		<h1> List of Authors: </h1>
+	</div>
+</div>
+
+<?php
+if ($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) { ?>
+	
+		
+	<div class="genre-container">
+	<div class="genre-item">
+	<p>
+	<a href="author.php?author=<?php echo $row["last_name"] ?>"> <?php echo $row["first_name"] . " " . $row["last_name"] ?> </a>
+	</p>
+	</div>
+	</div>
+<?php
+	}	
+
+} 
+ else {
+
+	echo "No authors found";
+} ?>
+
+</br>
+</br>	
+
+<?php include 'footer.php';?>
+</body>
+</html>
