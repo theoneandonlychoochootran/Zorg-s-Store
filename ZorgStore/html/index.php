5481a48a4abdac9ed7105d0d3fd8420209732678
jonbrashears <36751060+jonbrashears@users.noreply.github.com>
2018-04-20 08:44:09 -0500

    Added Carousel

--- ZorgStore/html/index.php
+++ ZorgStore/html/index.php
@@ -7,36 +7,53 @@
 		</div>
 	</div>
 
-<div class="container">    
-  <div class="row">
-    <div class="col-sm-4">
-	<?php $id = 1;
-	  $sql = "SELECT book_pic FROM books WHERE book_id='1'";
-	  $result = $conn->query($sql);
-	  $row = $result->fetch_assoc();
-	?>
-      <a href="product.php?id=<?php echo $id ?>"> <?php echo '<img src="data:image/jpeg;base64,'.base64_encode( $row["book_pic"]).'" width="160" height="240"/>';?> </a>
-    </div>
-    <div class="col-sm-4"> 
-      <?php $id = 2;
-	  $sql = "SELECT book_pic FROM books WHERE book_id='$id'";
-	  $result = $conn->query($sql);
-	  $row = $result->fetch_assoc();
-	?>
-      <a href="product.php?id=<?php echo $id ?>"> <?php echo '<img src="data:image/jpeg;base64,'.base64_encode( $row["book_pic"]).'" width="160" height="240"/>';?> </a>
-    </div>
-    <div class="col-sm-4"> 
-      <?php $id = 3;
-	  $sql = "SELECT book_pic FROM books WHERE book_id='$id'";
-	  $result = $conn->query($sql);
-	  $row = $result->fetch_assoc();
-	?>
-      <a href="product.php?id=<?php echo $id ?>"> <?php echo '<img src="data:image/jpeg;base64,'.base64_encode( $row["book_pic"]).'" width="160" height="240"/>';?> </a>
-    </div>
-  </div>
-  
-</div>
+<div class="carousel-wrap">
+<div id="zorgcarousel" class="carousel slide" data-ride="carousel" data-interval="3500">
 
+
+  <ul class="carousel-indicators">
+  <?php $id = 1;
+	  $sql = "SELECT book_pic, title, book_id FROM books WHERE book_id='1'";
+	  $result = $conn->query($sql);
+	  $row1 = $result->fetch_assoc();
+	?>
+    <li data-target="#zorgcarousel" data-slide-to="0" class="active"></li>
+	<?php $id = 2;
+	  $sql = "SELECT book_pic, title, book_id FROM books WHERE book_id='2'";
+	  $result = $conn->query($sql);
+	  $row2 = $result->fetch_assoc();
+	?>
+    <li data-target="#zorgcarousel" data-slide-to="1"></li>
+	<?php $id = 3;
+	  $sql = "SELECT book_pic, title, book_id FROM books WHERE book_id='3'";
+	  $result = $conn->query($sql);
+	  $row3 = $result->fetch_assoc();
+	?>
+    <li data-target="#zorgcarousel" data-slide-to="2"></li>
+  </ul>
+  
+
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <a href="product.php?id=<?php echo $row1["book_id"] ?>"><?php echo '<img src="data:image/jpeg;base64,'.base64_encode($row1["book_pic"]).'" alt="$row1["title"] width="300" height="450"/>' ?> 
+    </div>
+    <div class="carousel-item">
+      <a href="product.php?id=<?php echo $row2["book_id"] ?>"><?php echo '<img src="data:image/jpeg;base64,'.base64_encode($row2["book_pic"]).'" alt="$row2["title"] width="300" height="450"/>' ?>
+    </div>
+    <div class="carousel-item">
+      <a href="product.php?id=<?php echo $row3["book_id"] ?>"><?php echo '<img src="data:image/jpeg;base64,'.base64_encode($row3["book_pic"]).'" alt="$row3["title"] width="300" height="450"/>' ?>
+    </div>
+  </div>	
+  
+
+  <a class="carousel-control-prev" href="#zorgcarousel" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#zorgcarousel" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+</div>
 </br>
 
 </body>
