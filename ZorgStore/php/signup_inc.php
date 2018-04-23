3625ee24783bf76c1ba4ca4441cb1e3898d353ce
Oracle <justin.a.bennett@eagles.usm.edu>
2018-04-23 00:18:12 -0500

    changed so that usernames cannot contain special characters except underscore.

--- ZorgStore/php/signup_inc.php
+++ ZorgStore/php/signup_inc.php
@@ -20,19 +20,20 @@ if (isset($_POST['signup']))
 	exit();
 		
 	}
-	elseif($upwd != $pwd2)
+	if($upwd != $pwd2)
 	{
 		$_SESSION['msg'] = '<br /><font style="Cooper Black" color="#FF0000">Passwords do not match.</font>';
 		header("Location: ./../html/signup.php?login=badPwd2");
 		exit();
 	}
-	else
+	// We check to make sure that the email is valid	
 	if(!filter_var($uemail,FILTER_VALIDATE_EMAIL))
 	{
 		$_SESSION['msg'] = '<br /><font style="Cooper Black" color="#FF0000">Please enter a valid e-mail. </font>';
 		header("Location: ./../html/signup.php?login=invalid2");
 		exit();
 	}
+	// We check to make sure that password is not too short. If it is, we fall out and generate an error message. 
 	elseif (strlen($upwd) < 8)
 	{
 		$_SESSION['msg'] = '<br /><font style="Cooper Black" color="#FF0000">Password is too short. Please enter a password of at least 8 characters. </font>';
@@ -41,16 +42,28 @@ if (isset($_POST['signup']))
 	}
 	else
 	{
+		// Open our database and get information from it. This checks to make sure that username is not taken to disallow duplicate entries. 
 		$sql = "SELECT * FROM customers WHERE user_id ='$uid'";
 		$result = mysqli_query($conn, $sql);
 		$rCheck = mysqli_num_rows($result);
 		
+		// First we check for duplicate entries. 
 		if ($rCheck > 0)
 		{
 			$_SESSION['msg'] = '<br /><font style="Cooper Black" color="#FF0000">Username already exists. Please choose another.</font>';
-			header("Location: ./../html/signup.php?login=error1");
+			header("Location: ./../html/signup.php?login=error");
 			exit();
 		}
+		
+		// If no duplicate entries, then we move on and make sure that the username is valid. 
+		elseif(!preg_match('/^[0-9a-z_]+$/i',$uid))
+		{
+			$_SESSION['msg'] = '<br /><font style="Cooper Black" color="#FF0000">Username contains invalid characters (Alphanumeric and underscore only). Please choose another.</font>';
+			header("Location: ./../html/signup.php?login=error");
+			exit();
+		}
+		
+		// If it is, we make sure other inputs are valid. 
 		else
 		{
 			if (!preg_match('/^[0-9]{5}$/',$zip))
@@ -67,6 +80,7 @@ if (isset($_POST['signup']))
 			// Our SQL statement for inserting into table. 
 			$sql = "INSERT INTO customers (user_id, password, email, city, state, zip_code, salt) VALUES ('$uid', '$saltPW', '$uemail', '$city', '$state', '$zip' ,'$salt')";
 			
+			// We insert into the table. If it does not work, we throw an error stating that we can't connect to db. 
 			if(!mysqli_query($conn,$sql))
 			{
 				$_SESSION['msg'] = '<br /><font style="Cooper Black" color="#FF0000">Unable to connect to database. Please try again later. </font>';
@@ -89,5 +103,7 @@ if (isset($_POST['signup']))
 else 
 	header("Location: ./../html/signup.php");
 	exit();
+
+	
 
 ?>
